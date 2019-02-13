0849a733866850048b33ad9cdc6ffe725243d5d9
Velissel <william.wenyan@gmail.com>
2019-02-13 21:15:49 +1100

    added methods to Board class for BFS

--- src/models/Board.js
+++ src/models/Board.js
@@ -1,11 +1,12 @@
 export default class Board {
-    constructor(size) {
+    constructor(size, pieces) {
         this.size = size;
-        this.pieces = [];
+        this.pieces = pieces || new Map();
     }
 
     /**
      * put a piece into board
+     * expect KNIGHT
      * @param {string} name piece
      * @param {number} x    
      * @param {number} y    
@@ -14,8 +15,7 @@ export default class Board {
         if (x >= this.size || y >= this.size) {
             throw new Error('INVALID_PIECE_LOCATION');
         }
-        this.pieces[x] = this.pieces[x] || [];
-        this.pieces[x][y] = name;
+        this.pieces.set(`${x},${y}`, name);
     }
 
     /**
@@ -26,9 +26,90 @@ export default class Board {
      * @return {string}   name of piece
      */
     getPiece(x, y) {
-        if (!this.pieces[x]) {
-            return undefined;
-        }
-        return this.pieces[x][y];
+        return this.pieces.get(`${x},${y}`);
     }
+
+    removePiece(x, y) {
+        return this.pieces.delete(`${x},${y}`);
+    }
+
+    getSuccessors() {
+        const successors = [];
+
+        for(const [key, piece] of this.pieces) {
+            // moves for knight
+            if (piece === 'KNIGHT') {
+                const x = Number(key.split(',')[0]);
+                const y = Number(key.split(',')[1]);
+
+                if (x + 2 < this.size) {
+                    if (y + 1 < this.size && !this.pieces.has(`${x+2},${y+1}`)) {
+                        successors.push(createSuccessor(this, 'KNIGHT', {x,y}, {x: x+2, y: y+1}));
+                    }
+                    if (y - 1 >= 0 && !this.pieces.has(`${x+2},${y-1}`)) {
+                        successors.push(createSuccessor(this, 'KNIGHT', {x,y}, {x: x+2, y: y-1}));
+                    }
+                }
+
+                if (x - 2 >= 0) {
+                    if (y + 1 < this.size && !this.pieces.has(`${x-2},${y+1}`)) {
+                        successors.push(createSuccessor(this, 'KNIGHT', {x,y}, {x: x-2, y: y+1}));
+                    }
+                    if (y - 1 >= 0 && !this.pieces.has(`${x-2},${y-1}`)) {
+                        successors.push(createSuccessor(this, 'KNIGHT', {x,y}, {x: x-2, y: y-1}));
+                    }
+                }
+
+                if (x + 1 < this.size) {
+                    if (y + 2 < this.size && !this.pieces.has(`${x+1},${y+2}`)) {
+                        successors.push(createSuccessor(this, 'KNIGHT', {x,y}, {x: x+1, y: y+2}));
+                    }
+                    if (y - 2 >= 0 && !this.pieces.has(`${x+1},${y-2}`)) {
+                        successors.push(createSuccessor(this, 'KNIGHT', {x,y}, {x: x+1, y: y-2}));
+                    }
+                }
+
+                if (x - 1 >= 0) {
+                    if (y + 2 < this.size && !this.pieces.has(`${x-1},${y+2}`)) {
+                        successors.push(createSuccessor(this, 'KNIGHT', {x,y}, {x: x-1, y: y+2}));
+                    }
+                    if (y - 2 >= 0 && !this.pieces.has(`${x-1},${y-2}`)) {
+                        successors.push(createSuccessor(this, 'KNIGHT', {x,y}, {x: x-1, y: y-2}));
+                    }
+                }
+            }
+
+            // moves for other pieces...
+        }
+
+        return successors;
+    }
+
+    clone() {
+        return new Board(this.size, new Map(this.pieces));
+    }
+
+    isEqual(board) {
+        if (board.size !== this.size) {
+            return false;
+        }
+
+        for(const [key, piece] of this.pieces) {
+            if (!board.pieces.has(key)) {
+                return false;
+            }
+            if (board.pieces.get(key) !== piece) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+function createSuccessor(board, piece, from, to) {
+    const clone = board.clone();
+    clone.removePiece(from.x, from.y);
+    clone.setPiece(piece, to.x, to.y);
+    return clone;
 }
